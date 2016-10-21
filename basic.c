34dddd04e752f694e50e071ef9910553685012b6
induja-1413034 <induja26.1997@gmail.com>
2016-10-21 12:27:40 +0530

    Add files via upload

---
+++ basic.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+if(a>0)
+printf("\npositive:");
+else 
+if(a<0)
+printf("\nnegative");
+else
+printf("\nzero:");
+getch();
+}
