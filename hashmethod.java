0673d3a09aeb1d5e44026d4f0965c0d143874372
induja-1413034 <induja26.1997@gmail.com>
2017-07-20 12:00:40 +0530

    Add files via upload

---
+++ hashmethod.java
@@ -0,0 +1,18 @@
+package GuviLogic;
+
+import java.util.Scanner;
+
+public class hashmethod {
+public static void main(String[] args){
+	int a;
+	Scanner s=new Scanner(System.in);
+	a=s.nextInt();
+	for(int i=1;i<=a;i++){
+		for(int k=1;k<=i;k++)
+		{
+		System.out.print("#");
+		}
+	System.out.println(" ");
+		}
+	}
+}
